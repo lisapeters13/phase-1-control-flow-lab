26e955c783fa437ffbabe60b08762fd1861041a1
lisapeters13 <lisapetersirl@gmail.com>
2024-03-08 10:47:15 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  if (rideDistance <= 400) {
+    return "This one is on me!";
+  } else if (rideDistance >= 400 && rideDistance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (rideDistance >= 2000 && rideDistance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (rideDistance > 2500) {
+    return "No can do.";
+  }
 }
-
-function switchOnCharmFromTip(){
+  function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
   // Write your code here!
+
+
+function switchOnCharmFromTip(tip) {
+  return tip === "generous" ? "Thank you so much." : tip === "not as generous" ? "Thank you." : "Bye.";
 }
